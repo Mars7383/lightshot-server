17c15fa76725fb13f79f7eb8457cc5700f302eac
Mars <72959419+Mars7383@users.noreply.github.com>
2021-05-25 15:13:40 -0400

    Initial Commit
    Modified indilo53's Private LightShot Server to upload to pays.host

--- README.md
+++ README.md
@@ -1,14 +1,44 @@
-# LightShot Server
+# Lightshot to pays.host Bridge
+Lightshot doesn't provide a way to use pays.host, but we can override the hosts file to intercept the screenshot and upload it ourselves.
 
-## Configuration
+This was made in ~30 mins so it's probably buggy. It also uses some bad workarounds.
+<details>
+  <summary>More info on that</summary>
+  
+  ### Lightshot thumbnail files
+  Lightshot likes to make 180x180 "thumbnail" files along with your screenshot, so this program just blocks 180x180 size files. 
+  
+  Additionally, this program blocks JPG files from being uploaded since only thumbnails use those. If you need to upload JPG files for whatever reason, you can remove that one-liner from the ``index.js`` file, but you may experience duplicate uploads to pays.host
 
+  ### Dupe files
+  If your screenshot is under 180x180, the thumbnail file is just a duplicate of your screenshot. So this program blocks a file from uploading if it's hash matches that of the previous file.
+</details>
+
+***
+
+## Why
+macOS and Linux don't have ShareX, and some of the alternatives are kinda bad
+
+## Server Configuration
+
+Grab required modules with ``npm i fs express express-fileupload uid image-size node-fetch form-data crypto``
+
+In ``config.json``:
 ```
-listenPort => Port to listen to
-uploadDir  => Where to upload files
-baseUrl    => URL generated to point to uploaded file
-
-Add line to %windir%\system32\drivers\etc\hosts to point upload.prntscr.com to your ip
-
-example : 127.0.0.1 upload.prntscr.com
+listenPort => Port to listen to (80 is reccomended)
+uploadDir => Where to temporarily store screenshots (they will be deleted after uploading to pays.host)
+key => Your pays.host key
 ```
+## Client Configuration
 
+Add a line to your hosts file to point ``upload.prntscr.com`` to your server machine (example: ``127.0.0.1 upload.prntscr.com``)
+
+The hosts file can be found at:
+
+macOS/Linux: ``/etc/hosts``
+
+Windows: ``C:\Windows\System32\drivers\etc\hosts`` but if you're on Windows please just use ShareX
+
+***
+
+This is a fork of [indilo53's Private LightShot Server](https://github.com/indilo53/lightshot-server)
--- index.js
+++ index.js
@@ -1,57 +1,91 @@
-const fs         = require('fs');
-const express    = require('express');
-const fileUpload = require('express-fileupload');
-const app        = express();
-const uid        = require('uid');
-const config     = require('./config.json');
- 
-app.use(fileUpload());
- 
-app.post('/upload/:a/:b/', function(req, res) {
+const fs = require("fs");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const uid = require("uid");
+const sizeOf = require("image-size");
+const fetch = require("node-fetch");
+const FormData = require("form-data");
+const crypto = require('crypto');
+const config = require("./config.json");
 
-  if (!req.files)
-    return res.status(500).send('No file uploaded');
+let recentFile = ""; // Bad workaround to prevent dupe files but it works
+
+const app = express();
+app.use(fileUpload());
+app.post("/upload/:a/:b/", function (req, res) {
+  if (!req.files) return res.status(500).send("No file uploaded");
 
   let newFileName;
 
-  for(let k in req.files) {
-    if(req.files.hasOwnProperty(k)) {
+  for (let k in req.files) {
+    if (req.files.hasOwnProperty(k)) {
+      (file => {
+        console.log(file.name);
+        if (file.name.indexOf("thumb") == -1) {
+          const split = file.name.split(".");
+          const ext = split.pop();
+          if (ext == "jpg") return; // Lightshot should only capture PNG files, JPG is likely a thumbnail/dupe
+          let fileName = uid(8) + "." + ext;
+          let count = 1;
 
-      ((file) => {
-
-        if(file.name.indexOf('thumb') == -1) {
-
-          const split = file.name.split('.');
-          const ext   = split.pop();
-          fileName    = uid(8) + '.' + ext;
-          let count   = 1;
-
-          while(fs.existsSync(config.uploadDir + '/' + fileName))
+          while (fs.existsSync(config.uploadDir + "/" + fileName))
             fileName = `${uid(8)}.${ext})`;
 
-          file.mv(config.uploadDir + '/' + fileName, function(err) {
-            if (err)
-              return res.status(500).send(err);
+          file.mv(config.uploadDir + "/" + fileName, function (err) {
+            if (err) return res.status(500).send(err);
+            let filePath = config.uploadDir + "/" + fileName;
+            sizeOf(filePath, function (err, dimensions) {
+              if (dimensions.width == 180 && dimensions.height == 180) { // Lightshot thumbnails are 180x180, so we ignore these to prevent double uploads
+                console.log("Thumbnail detected");
+                fs.unlinkSync(filePath);
+                //res.status(500).send("Thumbnail disallowed");
+                return;
+              } else {
+
+                const form = new FormData();
+                const buffer = fs.readFileSync(filePath);
+
+                const hash = crypto.createHash('sha256').update(buffer).digest('hex');
+                if (hash == recentFile) {
+                  console.log("Duplicate detected");
+                  fs.unlinkSync(filePath);
+                  //res.status(500).send("Duplicate file");
+                  return;
+                }
+                recentFile = hash;
+
+                form.append("file", buffer, {
+                  contentType: "image/png",
+                  name: "file",
+                  filename: "file.png"
+                });
+                form.append("key", config.key); // pays.host key
+
+                fetch("https://pays.host/api/images/upload", {
+                  method: "POST",
+                  body: form
+                })
+                  .then(res => res.json())
+                  .then(json => {
+                    newFileName = json.url;
+                    const url = newFileName;
+                    res.send(`
+                  <response>
+                    <status>success</status>
+                    <url>${url}</url>
+                    <thumb>${url}</thumb>
+                  </response>
+                  `);
+                  })
+                  .then(() => { fs.unlinkSync(filePath) });
+
+              }
+            });
           });
-
-          newFileName = fileName;
         }
-
       })(req.files[k]);
-
     }
   }
-
-  const url = config.baseUrl.replace('{{filename}}', newFileName);
-
-  res.send(
-`<response>
-  <status>success</status>
-  <url>${url}</url>
-  <thumb>${url}</thumb>
-</response>
-`);
-
 });
 
 app.listen(config.listenPort);
